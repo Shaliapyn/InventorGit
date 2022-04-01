b656dc398c638494dafd77d180ea8d0de60a2210
Ihor Shalapa <igorshalapa@gmail.com>
2022-04-01 21:19:38 +0300

    complete task1

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,32 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  str = str.split(' ')
+  let [left, act, right] = str;
+  if (act == "+"){
+      str = left + right
+  } else if (act == "-") {
+      let lengthDif = left.length - right.length
+      if (left.length <= right.length){
+          str = ""
+      } else {
+          str = '';
+          for (let i = 0; i < lengthDif; i++){
+              str += "."
+          }
+      } 
+  } else if (act == '*'){
+      str = left.repeat(right.length)
+  } else if (act == '//') {
+      if (left.length <= right.length){
+          str = ""
+      }  else {
+          let lengthDevide = Math.floor(left.length / right.length)
+          str = ""
+          for (let i = 0; i < lengthDevide; i++){
+              str += "."
+          }
+      }       
+  }
+  return str
 }
+
+console.log(calculate("... - .."))
